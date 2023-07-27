93ee288d9bc03523a269a5dab12594b7bf510cb3
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2023-07-27 13:03:35 -0500

    Add project files.

---
+++ HWList/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+List<string> students = new List<string>();
+
+bool stopAddingStudents = false;
+
+do
+{
+	Console.WriteLine("Hello Please add a student"); 
+	string student = Console.ReadLine();
+    students.Add(student);
+    Console.WriteLine("If you would like to stop adding students please press E, else hit enter to continue");
+    string? exitProgram = Console.ReadLine();
+    if (exitProgram.ToLower() == "e")
+    {
+        stopAddingStudents = true;
+    }
+} while (!stopAddingStudents);
+
